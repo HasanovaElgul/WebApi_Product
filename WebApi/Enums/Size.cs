babe5473c4102cc2d42d04e9052b5c634caec426
elgul.hesenova <elgul.hesenova@PC-1-191>
2024-07-15 17:54:45 +0400

    123

---
+++ WebApi/Enums/Size.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.Enums
+{
+    public enum CosmeticsVolume
+    {
+        Small = 15,  //15 ml
+        Medium = 16,  //30 ml
+        Large = 17,          //50 ml
+        ExtraLaege = 100     //100 ml
+      
+    }
+}
