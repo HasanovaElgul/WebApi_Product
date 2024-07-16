a7056eefe9ca4d6412c54b8eb1238139175f4c7a
elgul.hesenova <elgul.hesenova@PC-1-191>
2024-07-16 16:23:16 +0400

    DataBase push

---
+++ WebApi/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        { 
+            _context = context;
+        }
+
+        public IActionResult Post(Product product)
+        { 
+          _context.Products.Add(product);
+            _context.SaveChanges();
+            return Ok();     // 200
+        }
+
+    }
+}
