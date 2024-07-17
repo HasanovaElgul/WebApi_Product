2c8d06dabea81bc3b15b98507a402a80cba9aaca
elgul.hesenova <elgul.hesenova@PC-1-191>
2024-07-17 17:39:59 +0400

    Product

--- WebApi/Controllers/ProductController.cs
+++ WebApi/Controllers/ProductController.cs
@@ -1,27 +1,62 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using WebAPI.Data;
 using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
+    [ApiController]  //автоматическая валидация модели и обработка ошибок
     public class ProductController : ControllerBase
     {
-        private readonly AppDbContext _context;
+        private readonly AppDbContext _context;                  //private доступны только внутри класса 
 
         public ProductController(AppDbContext context)
-        { 
+        {
             _context = context;
         }
 
+        [HttpPost]  // HTTP POST запрос  создать
         public IActionResult Post(Product product)
-        { 
-          _context.Products.Add(product);
-            _context.SaveChanges();
-            return Ok();     // 200
+        {
+            _context.Products.Add(product);     //Добавляем продукт в контекст данных
+            _context.SaveChanges();                 // Сохраняем изменения в базе данных
+            return Ok();                     // Возвращаем статус 200 OK
         }
 
+        [HttpGet]                       //получения данных с сервера
+        public IActionResult GetAll()           // Обрабатывает HTTP GET запросы.
+        {
+            var data = _context.Products.ToList();            //Извлекает все продукты из базы данных с использованием контекста данных (_context).
+            return Ok(data);                                  //Возвращает список продуктов в формате JSON с HTTP статусом 200 OK.
+
+        }
+        [HttpGet("GetById/{id}")]
+        public IActionResult Get([FromRoute]int id)
+        {
+            var data = _context.Products.FirstOrDefault(x => x.Id == id);
+            return Ok(data);
+
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//private: доступен только внутри класса ООП
+//public: Доступен из любой части программы.
+//protected: Доступен внутри текущего класса и в производных классах.
+//internal: Доступен внутри текущей сборки (assembly).
+//protected internal: Доступен внутри текущей сборки и в производных классах.
--- WebApi/Models/Product.cs
+++ WebApi/Models/Product.cs
@@ -2,14 +2,13 @@
 
 namespace WebAPI.Models
 {
-    public class Product
+    public class Product                                      //доступность из любой части программы.
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public List<int> Sizes { get; set; }
-
 
     }
 }
